ecf4d66b4fe0afe25093009cf9ddeea8432bb84a
jaxx2104 <jaxx2104@gmail.com>
2019-06-16 04:06:12 +0900

    :truck: Rename babelrc.js

--- .babelrc.js
+++ .babelrc.js
@@ -1,9 +1,9 @@
-{
-  "presets": [
+module.exports = {
+  presets: [
     "@babel/preset-env"
   ],
-  "comments": false,
-  "plugins": [
+  comments: false,
+  plugins: [
     "add-module-exports",
     "@babel/plugin-proposal-object-rest-spread",
     "@babel/plugin-proposal-class-properties"
