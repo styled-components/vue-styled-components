3c24afafa84dfc8b9dae244650f54cdbc7b7fb7d
ka2jun8 <ka210n8@gmail.com>
2019-06-22 19:47:55 +0900

    check tag type in create styled component

--- src/constructors/styled.js
+++ src/constructors/styled.js
@@ -1,11 +1,29 @@
 import css from './css'
 import domElements from '../utils/domElements'
 
+// check valid vue-styled-component element type
+function isValidElementType (tag) {
+  if (typeof tag === 'undefined' || typeof tag === 'number') {
+    return false
+  }
+  if (typeof tag === 'string') {
+    return domElements.includes(tag)
+  }
+  if (typeof tag === 'object') {
+    return !!tag.template || !!tag.withComponent
+  }
+  return true
+}
+
 export default (createStyledComponent) => {
-  const styled = (tagName, props = {}) =>
-    (cssRules, ...interpolations) => (
+  const styled = (tagName, props = {}) => {
+    if (!isValidElementType(tagName)) {
+      throw new Error(tagName + ' is not allowed for styled tag type.')
+    }
+    return (cssRules, ...interpolations) => (
       createStyledComponent(tagName, css(cssRules, ...interpolations), props)
     )
+  }
 
   domElements.forEach((domElement) => {
     styled[domElement] = styled(domElement)
