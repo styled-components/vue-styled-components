2167985c8704df16be3c8d8b739a508ca9e592c7
Lorenzo Girardi <lorenzo.girardi93@gmail.com>
2019-08-05 21:43:09 +0200

    [+] Merged #80

--- src/constructors/styled.js
+++ src/constructors/styled.js
@@ -1,19 +1,6 @@
 import css from './css'
 import domElements from '../utils/domElements'
-
-// check valid vue-styled-component element type
-function isValidElementType (tag) {
-  if (typeof tag === 'undefined' || typeof tag === 'number') {
-    return false
-  }
-  if (typeof tag === 'string') {
-    return domElements.includes(tag)
-  }
-  if (typeof tag === 'object') {
-    return !!tag.template || !!tag.withComponent
-  }
-  return true
-}
+import isValidElementType from '../utils/isValidElementType'
 
 export default (createStyledComponent) => {
   const styled = (tagName, props = {}) => {
