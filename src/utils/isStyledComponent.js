1e0070b8487e6cc35830fc7f33239e1c2c42d0d9
zuxriddinkamalov <bigjoss@mail.ru>
2019-10-21 13:37:16 +0500

    Fix bug when component has initial methods. generateAndInjectStyles bug

--- src/utils/isStyledComponent.js
+++ src/utils/isStyledComponent.js
@@ -1,5 +1,6 @@
 export default function isStyledComponent (target) {
   return target &&
     target.methods &&
+    'generateAndInjectStyles' in target.methods &&
     typeof target.methods.generateAndInjectStyles() === 'string'
 }
